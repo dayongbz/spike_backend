a25fec5673da9ce6e9f648631b8b3e9f19c973ad
Dayong Lee <50468628+dayongbz@users.noreply.github.com>
2020-12-13 14:42:50 +0900

    https

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const dotenv = require('dotenv');
 const session = require('express-session');
+const redirector = require('redirect-https');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 const usersRouter = require('./router/users');
 const emailverifyRouter = require('./router/emailverify');
 const loginRouter = require('./router/login');
 const passportConfig = require('./passport');
+const httpsOptions = {};
 
 dotenv.config();
 
 const app = express();
-const port = 3001;
+const dev = process.env.NODE_ENV !== 'production';
 const sess = {
   secret: process.env.SECRET,
   resave: false,
@@ -22,9 +26,21 @@ const sess = {
   cookie: {},
 };
 
-if (process.env.NODE_ENV === 'production') {
+if (!dev) {
   app.set('trust proxy', 1);
   sess.cookie.secure = true;
+  httpsOptions.ca = fs.readFileSync(
+    '/etc/letsencrypt/live/dayong.xyz/fullchain.pem',
+  );
+  httpsOptions.key = fs.readFileSync(
+    '/etc/letsencrypt/live/dayong.xyz/privkey.pem',
+  );
+  httpsOptions.cert = fs.readFileSync(
+    '/etc/letsencrypt/live/dayong.xyz/cert.pem',
+  );
+} else {
+  httpsOptions.key = fs.readFileSync('./devKey/private.pem');
+  httpsOptions.cert = fs.readFileSync('./devKey/public.pem');
 }
 
 passportConfig();
@@ -36,11 +52,27 @@ app.use(session(sess));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
+// app.use('/', redirector);
+
+app.all('*', (req, res, next) => {
+  let protocol = req.headers['x-forwarded-proto'] || req.protocol;
+  if (protocol == 'https') {
+    next();
+  } else {
+    let from = `${protocol}://${req.hostname}${req.url}`;
+    let to = `https://${req.hostname}${req.url}`;
+    // log and redirect
+    // console.log(`[${req.method}]: ${from} -> ${to}`);
+    res.redirect(to);
+  }
+});
 
 app.use('/users', usersRouter);
 app.use('/emailverify', emailverifyRouter);
 app.use('/login', loginRouter);
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+http.createServer(app).listen(80, () => {
+  console.log('start http server');
+});
+https.createServer(httpsOptions, app).listen(443, () => {
+  console.log('start https server');
 });
